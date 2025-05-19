56ee45161a1f70c8c2ff40880d8547039af9682f
GGB0T11 <gusttrabs@gmail.com>
2025-05-19 15:42:26 -0300

    organizacao do codigo

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
-import os
+from os import makedirs
 
 #Função para calcular quantidade de valores nulos nas colunas em %
-def calc_nulls(df):
-    return ((df.isnull().sum()) / df.shape[0] * 100).round(2)
+def calc_nulls(df, export: bool):
+    nulls = pd.DataFrame({
+        "columns": df.columns,
+        "nulls": ((df.isnull().sum()) / df.shape[0] * 100).round(2)
+    })
+    if not export:
+        nulls.to_csv("csvs/Nulos.csv", index=False)
+        return
+    return nulls
 
-def films_per_decade(df):
-    # Criar intervalos de 10 anos
+def films_per_decade(df, export: bool):
+    # Criando intervalos de 10 anos
     last_year = df["year"].max()
     bins = list(range(df["year"].min(), df["year"].max(), 10)) + [last_year]
     labels = [f"{start}-{start+9}" if start+9 < last_year else f"{start}-{last_year}" for start in bins[:-1]]
-
     # Agrupar por década
     df["decade"] = pd.cut(df["year"], bins=bins, labels=labels, right=False)
-    count = df["decade"].value_counts().sort_index()
-    return count.reset_index()
+    count = df["decade"].value_counts().sort_index().reset_index()
+    if export:
+        count.to_csv("csvs/FilmesPorDecada.csv", index=False)
+        return
+    return count
 
 def plot_bar(df):
     sns.set_theme(style="whitegrid")
@@ -33,8 +41,12 @@ def plot_bar(df):
     plt.savefig("graficos/FilmesDecada.png")        
     return
 
-# Lendo o dataset
-df = pd.read_csv("imdb_filmes.csv")
+if __name__ == "__main__":
+    makedirs("csvs", exist_ok=True)
+    makedirs("graficos", exist_ok=True)
 
-fpd = films_per_decade(df)
-plot_bar(fpd)
+    # Lendo o dataset
+    df = pd.read_csv("imdb_filmes.csv")
+    
+    calc_nulls(df, export=True)
+    films_per_decade(df, export=True)
